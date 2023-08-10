742080b1516931925905febcbd4899959e4bed4e
Traae <traaebloxham@gmail.com>
2023-08-10 14:59:05 -0600

    It ain't much, but I got something done.

--- core/src/com/ourgdx/game/LevelSystem/Mapping/Level.java
+++ core/src/com/ourgdx/game/LevelSystem/Mapping/Level.java
@@ -1,5 +1,10 @@
 package com.ourgdx.game.LevelSystem.Mapping;
 
 public class Level {
-
+    // Layout of Tiles
+    // Tile Sprite Manifest
+    // Tile Dictionary
+    // Layout of Entities
+    // Entity Manifest
+    // Entity Dictionary
 }
